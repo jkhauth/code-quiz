2e3c949bdf615374bfafb3054e5cf0478a6c5883
John Hauth <jkhauth@gmail.com>
2020-12-01 20:11:11 -0500

    got random question to fill in

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 
+var quiz = document.getElementById("questionBox")
 
 function startQuiz() {
-    
-    var questionBox = document.getElementById("quesBox");
 
+    //WHEN BUTTON IS CLICKED HIDE CLASS GOES AWAY
+    quiz.classList.remove("hide")
+
+    //QUESTIONS AND ANSWERS
+    const questions = [
+        {
+            question: "What is 2+2",
+            answers: [
+                { text: "49", correct: true },
+                { text: "5", correct: false },
+                { text: "6", correct: false }
+            ]
+        },
+        {
+            question: "What is 23+2",
+            answers: [
+                { text: "48", correct: true },
+                { text: "5", correct: false },
+                { text: "6", correct: false }
+            ]
+        },
+        {
+            question: "What is 25+2",
+            answers: [
+                { text: "41", correct: true },
+                { text: "5", correct: false },
+                { text: "6", correct: false }
+            ]
+        }
+    ];
+
+    //QUESTIONS ARE RANDOMLY CHOSEN
+    randomQuestion();
+
+    function randomQuestion(){
+        shufflequest = questions.sort(() => Math.random() - .5);
+        currquestIndex = 0;
+        document.getElementById("questionTitle").innerText = shufflequest[0].question
+        var newanswer = document.createElement("button");
+        var answertext = document.createTextNode(shufflequest[0].answers.text);
+        document.getElementById("first-answer").innerText = newanswer.appendChild(answertext);
+        console.log(shufflequest[0].question);
+        console.log(answertext);
+    }
+    
+    
+    
+    //QUESTIONS AND ANSWERS TO QUIZ
 
     
+    // var questionBox = document.getElementById("option1");
 }
+
