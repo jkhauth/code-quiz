b46ea7bb22b1f5ec443302949c3c9803b3ebbf5f
jkhauth <jkhauth@gmail.com>
2020-12-02 17:23:27 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-
 var quiz = document.getElementById("questionBox");
 var start = document.getElementById("start-button");
 
-
-//GENERATES NEWS BUTTONS FOR ANSWERS
-
-
-// GENERATES NEW QUIZ QUESTION
+// STARTS QUIZ
 function startQuiz() {
 
-    //WHEN BUTTON IS CLICKED HIDE CLASS GOES AWAY
+    // WHEN START BUTTON IS CLICKED IT GOES AWAY
     quiz.classList.remove("hide")
     start.classList.add("hide");
 
-    //QUESTIONS AND ANSWERS
+    // QUESTIONS - ANSWERS - SCORE - TIME
     var questions = [
         {
             question: "ampihans?",
@@ -40,109 +35,111 @@ function startQuiz() {
             ]
         }
     ];
-    
+
     var gametime = document.getElementById("time");
     var secondsleft = 20;
-    
+
     var gamescore = document.getElementById("score");
     var score = 100
     gamescore.textContent = score
+
+
     thequiz();
 
     startTimer();
 
-        //GAME TIMER
-        function startTimer(){
-        var timerInterval = setInterval(function(){
+    // GAME TIMER
+    function startTimer() {
+        var timerInterval = setInterval(function () {
             secondsleft--;
             gametime.textContent = secondsleft + " seconds left"
-        
-        if(secondsleft === 5){
-            clearInterval(timerInterval);
-            document.getElementById("questionBox").classList.add("hide")
-        }
+
+            if (secondsleft === 5) {
+                clearInterval(timerInterval);
+                document.getElementById("questionBox").classList.add("hide")
+            }
         }, 1000);
-    
+
     }
 
-        // FIRST SET OF QUESTIONS
-        function thequiz(){
-            
-            //FILLS IN RESPECTED INFORMATION TO BEGIN GAME
+    // IF WRONGANS SELECTED TIME IS SUBTRACTED AND SCORE DECREASED
+    function wrongAns() {
+    secondsleft -= 5;
+    score -= 10;
+    gamescore.textContent = score
+    }
+    
+    // FIRST SET OF QUESTIONS
+function thequiz() {
+
+    // FILLS IN RESPECTED INFORMATION TO BEGIN GAME
+    for (let i = 0; i < 3; i++) {
+            document.getElementById("questionTitle").innerHTML = questions[0].question;
+            var answerbtn1 = document.createElement("button");
+            answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
+            answerbtn1.innerHTML = questions[0].answers[i].text;
+            answerbtn1.value = questions[0].answers[i].correct;
+            answerbtn1.setAttribute("id", questions[0].answers[i].text);
+            document.getElementById("answers").appendChild(answerbtn1);
+    }
+
+    // IF FIRST ANSWER IS MOVE TO NEXT QUESTIONS
+    if (
+        document.getElementById("frogs").addEventListener("click", function () {wrongAns()},function(){nextQues}),
+        document.getElementById("lizards").addEventListener("click", function () { nextQues() }),
+        document.getElementById("tadpols").addEventListener("click", function () { nextQues() })
+        );
+
+
+
+    // MOVES TO NEXT SET OF QUESTIONS
+    function nextQues() {
+
+        document.getElementById("frogs").classList.add("hide");
+        document.getElementById("lizards").classList.add("hide");
+        document.getElementById("tadpols").classList.add("hide");
             for (let i = 0; i < 3; i++) {
-                document.getElementById("questionTitle").innerHTML = questions[0].question;
-                var answerbtn1 = document.createElement("button"); 
+                document.getElementById("questionTitle").innerHTML = questions[1].question;
+                var answerbtn1 = document.createElement("button");
                 answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-                answerbtn1.innerHTML = questions[0].answers[i].text;
-                answerbtn1.value = questions[0].answers[i].correct;
-                answerbtn1.setAttribute("id", questions[0].answers[i].text);
-                document.getElementById("answers").appendChild(answerbtn1);            
+                answerbtn1.innerHTML = questions[1].answers[i].text;
+                answerbtn1.value = questions[1].answers[i].correct;
+                answerbtn1.setAttribute("id", questions[1].answers[i].text);
+                document.getElementById("answers").appendChild(answerbtn1);
             }
 
-            //IF FIRST ANSWER IS MOVE TO NEXT QUESTIONS
             if (
-                document.getElementById("frogs").addEventListener("click", function(){wrongAns()}, function(){nextQues}),
-                document.getElementById("lizards").addEventListener("click", function(){nextQues()}),
-                document.getElementById("tadpols").addEventListener("click", function(){nextQues()})
+                document.getElementById("ff48").addEventListener("click", function () { nextQues2() }),
+                document.getElementById("fd5").addEventListener("click", function () { nextQues2() }),
+                document.getElementById("cheeeta6").addEventListener("click", function () { nextQues2() })
             );
-            
-                function wrongAns(){
-                secondsleft -= 5;
-                score -= 10;
-                gamescore.textContent = score
-                }
-            
-                function nextQues(){
 
-                document.getElementById("frogs").classList.add("hide");
-                document.getElementById("lizards").classList.add("hide");
-                document.getElementById("tadpols").classList.add("hide");
+            // MOVES TO NEXT SET OF QUESTIONS
+            function nextQues2() {
+                document.getElementById("ff48").classList.add("hide");
+                document.getElementById("fd5").classList.add("hide");
+                document.getElementById("cheeeta6").classList.add("hide");
 
                 for (let i = 0; i < 3; i++) {
-                    document.getElementById("questionTitle").innerHTML = questions[1].question;
-                    var answerbtn1 = document.createElement("button"); 
+                    document.getElementById("questionTitle").innerHTML = questions[2].question;
+                    var answerbtn1 = document.createElement("button");
                     answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-                    answerbtn1.innerHTML = questions[1].answers[i].text;
-                    answerbtn1.value = questions[1].answers[i].correct;
-                    answerbtn1.setAttribute("id", questions[1].answers[i].text);
-                    document.getElementById("answers").appendChild(answerbtn1);            
+                    answerbtn1.innerHTML = questions[2].answers[i].text;
+                    answerbtn1.value = questions[2].answers[i].correct;
+                    answerbtn1.setAttribute("id", questions[2].answers[i].text);
+                    document.getElementById("answers").appendChild(answerbtn1);
                 }
-                
-                if (
-                    document.getElementById("ff48").addEventListener("click", function(){nextQues2()}),
-                    document.getElementById("fd5").addEventListener("click", function(){nextQues2()}),
-                    document.getElementById("cheeeta6").addEventListener("click", function(){nextQues2()})
-                );
-                
-                function nextQues2(){
-    
-                    document.getElementById("ff48").classList.add("hide");
-                    document.getElementById("fd5").classList.add("hide");
-                    document.getElementById("cheeeta6").classList.add("hide");
-    
-                    for (let i = 0; i < 3; i++) {
-                        document.getElementById("questionTitle").innerHTML = questions[2].question;
-                        var answerbtn1 = document.createElement("button"); 
-                        answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-                        answerbtn1.innerHTML = questions[2].answers[i].text;
-                        answerbtn1.value = questions[2].answers[i].correct;
-                        answerbtn1.setAttribute("id", questions[2].answers[i].text);
-                        document.getElementById("answers").appendChild(answerbtn1);            
-                    }
 
-                
-                
-                
-                } 
-            }     
-        }
+            } //END OF nextQues2 FUNCTION
+    } // END OF nextQues FUNCTION
+} //END OF THEQUIZ FUNCTION
 
-        
-    
+
+
     console.log(document.getElementById("frogs").value)
     console.log(document.getElementById("tadpols").value)
-    console.log(document.getElementById("lizards").value)  
-        
+    console.log(document.getElementById("lizards").value)
+
 }
 
 
