d057918371d84381e613989b11fbd603d99e729e
John Hauth <jkhauth@gmail.com>
2020-12-08 10:41:42 -0500

    functionality fixed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//QUIZ SECTION
 var quiz = document.getElementById("questionBox");
 var start = document.getElementById("start-button");
 var gametime = document.getElementById("time");
@@ -6,15 +7,14 @@ var gamescore = document.getElementById("score");
 var scoreBoard = document.getElementById("scoreBoard")
 var score = 100
 gamescore.textContent = score
-
+//HIGHSCORE SECTION
 var highscores = [];
 var highscore = document.getElementById("highscore"); //form
 var highscoreInput = document.getElementById("highscoreInput"); //user input
 var saveHighscore = document.getElementById("saveHighscore"); //submit highscore
 var highscoreList = document.getElementById("scores"); // ul list
 var savedScores = localStorage.getItem("High Score")
-console.log(highscores)
-console.log(savedScores)
+
 
 var scoretextold = document.createElement("p");
 scoretextold.innerText = savedScores
@@ -41,145 +41,172 @@ highscoreList.appendChild(scoretext);
 }
 );
 
+
 // STARTS QUIZ
 function startQuiz() {
 
-    // WHEN START BUTTON IS CLICKED IT GOES AWAY
-    quiz.classList.remove("hide")
-    scoreBoard.classList.add("hide")
-    start.classList.add("hide");
-    highscoreInput.classList.add("hide");
-    saveHighscore.classList.add("hide");
+// WHEN START BUTTON IS CLICKED IT GOES AWAY
+quiz.classList.remove("hide")
+scoreBoard.classList.add("hide")
+start.classList.add("hide");
+highscoreInput.classList.add("hide");
+saveHighscore.classList.add("hide");
+
+// QUESTIONS - ANSWERS - SCORE - TIME
+var questions = [
+{
+question: "Which is NOT a program language",
+answers: [
+{ text: "HTML"},
+{ text: "CSS"},
+{ text: "ENGLISH"}
+]
+},
+{
+question: "Which is a tag for heading title?",
+answers: [
+{ text: "H1"},
+{ text: "P"},
+{ text: "DIV"}
+]
+},
+{
+question: "How do you call a class in CSS?",
+answers: [
+{ text: ".EXAMPLE"},
+{ text: "#EXAMPLE"},
+{ text: "EXAMPLE"}
+]
+}
+];
+
+thequiz();
+
+startTimer();
+
+// GAME TIMER
+function startTimer() {
+var timerInterval = setInterval(function () {
+secondsleft--;
+gametime.textContent = secondsleft + " seconds left";
+
+//DISPLAY HIGHSCORE WHEN TIME RUNS OUT
+if (secondsleft === 0) {
+clearInterval(timerInterval);
+scoreBoard.classList.remove("hide");
+highscoreInput.classList.remove("hide");
+saveHighscore.classList.remove("hide");
+}
+}, 1000);
+}
+
+// IF WRONGANS SELECTED TIME IS SUBTRACTED AND SCORE DECREASED
+function wrongAns() {
+secondsleft -= 5;
+score -= 10;
+gamescore.textContent = score
+}
+
+function rightAns() {
+score += 10;
+gamescore.textContent = score
+}
     
-    // QUESTIONS - ANSWERS - SCORE - TIME
-    var questions = [
-        {
-            question: "Which is NOT a program language",
-            answers: [
-                { text: "HTML"},
-                { text: "CSS"},
-                { text: "ENGLISH"}
-            ]
-        },
-        {
-            question: "Which is a tag for heading title?",
-            answers: [
-                { text: "H1"},
-                { text: "P"},
-                { text: "DIV"}
-            ]
-        },
-        {
-            question: "How do you call a class in CSS?",
-            answers: [
-                { text: ".EXAMPLE"},
-                { text: "#EXAMPLE"},
-                { text: "EXAMPLE"}
-            ]
-        }
-    ];
-
-    thequiz();
-
-    startTimer();
-
-    // GAME TIMER
-    function startTimer() {
-        var timerInterval = setInterval(function () {
-            secondsleft--;
-            gametime.textContent = secondsleft + " seconds left";
-
-            //DISPLAY HIGHSCORE WHEN TIME RUNS OUT
-            if (secondsleft === 0) {
-                   clearInterval(timerInterval);
-                    scoreBoard.classList.remove("hide");
-                    highscoreInput.classList.remove("hide");
-                    saveHighscore.classList.remove("hide");
-            }
-        }, 1000);
-
-    }
-
-    // IF WRONGANS SELECTED TIME IS SUBTRACTED AND SCORE DECREASED
-    function wrongAns() {
-    secondsleft -= 5;
-    score -= 10;
-    gamescore.textContent = score
-    }
-    function rightAns() {
-        score += 10;
-        gamescore.textContent = score
-        }
-    
-    // FIRST SET OF QUESTIONS
+// FIRST SET OF QUESTIONS
 function thequiz() {
 
-    // FILLS IN RESPECTED INFORMATION TO BEGIN GAME
-    for (let i = 0; i < 3; i++) {
-            document.getElementById("questionTitle").innerHTML = questions[0].question;
-            var answerbtn1 = document.createElement("button");
-            answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-            answerbtn1.innerHTML = questions[0].answers[i].text;
-            answerbtn1.value = questions[0].answers[i].correct;
-            answerbtn1.setAttribute("id", questions[0].answers[i].text);
-            document.getElementById("answers").appendChild(answerbtn1);
-    }
+// FILLS IN RESPECTED INFORMATION TO BEGIN GAME
+for (let i = 0; i < 3; i++) {
+document.getElementById("questionTitle").innerHTML = questions[0].question;
+var answerbtn1 = document.createElement("button");
+answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
+answerbtn1.innerHTML = questions[0].answers[i].text;
+answerbtn1.value = questions[0].answers[i].correct;
+answerbtn1.setAttribute("id", questions[0].answers[i].text);
+document.getElementById("answers").appendChild(answerbtn1);
+}
 
-    // IF FIRST ANSWER IS MOVE TO NEXT QUESTIONS
-    if (
-        document.getElementById("HTML").addEventListener("click", function () {rightAns()},function(){nextQues}),
-        document.getElementById("CSS").addEventListener("click", function () {wrongAns()},function(){nextQues}),
-        document.getElementById("ENGLISH").addEventListener("click", function () {wrongAns()},function(){nextQues})
-        );
+// IF FIRST ANSWER IS SELECTED, MOVE TO NEXT QUESTIONS
+if (
+document.getElementById("HTML").addEventListener("click", function () {nextQues()}),
+document.getElementById("HTML").addEventListener("click", function () {wrongAns()}),
+document.getElementById("CSS").addEventListener("click", function () {nextQues()}),
+document.getElementById("CSS").addEventListener("click", function () {wrongAns()}),
+document.getElementById("ENGLISH").addEventListener("click", function (){nextQues()}),
+document.getElementById("ENGLISH").addEventListener("click", function () {rightAns()})
+);
+
+// MOVES TO NEXT SET OF QUESTIONS
+function nextQues() {
+//HIDE PREVIOUS ANSWERS
+document.getElementById("HTML").classList.add("hide");
+document.getElementById("CSS").classList.add("hide");
+document.getElementById("ENGLISH").classList.add("hide");
+
+//LOAD NEW ANSWERS
+for (let i = 0; i < 3; i++) {
+document.getElementById("questionTitle").innerHTML = questions[1].question;
+var answerbtn1 = document.createElement("button");
+answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
+answerbtn1.innerHTML = questions[1].answers[i].text;
+answerbtn1.value = questions[1].answers[i].correct;
+answerbtn1.setAttribute("id", questions[1].answers[i].text);
+document.getElementById("answers").appendChild(answerbtn1);
+}
+
+//IF SECOND ANSWER IS SELECTED, MOVE TO NEXT QUESTIONS
+if (
+document.getElementById("H1").addEventListener("click", function () {nextQues2()}),
+document.getElementById("H1").addEventListener("click", function () {rightAns()}),
+document.getElementById("P").addEventListener("click", function () {nextQues2()}),
+document.getElementById("P").addEventListener("click", function () {wrongAns()}),
+document.getElementById("DIV").addEventListener("click", function () {nextQues2()}),
+document.getElementById("DIV").addEventListener("click", function () {wrongAns()})
+);
+
+// MOVES TO NEXT SET OF QUESTIONS
+function nextQues2() {
+document.getElementById("H1").classList.add("hide");
+document.getElementById("P").classList.add("hide");
+document.getElementById("DIV").classList.add("hide");
+
+//LOADED SECOND QUESTIONS
+for (let i = 0; i < 3; i++) {
+document.getElementById("questionTitle").innerHTML = questions[2].question;
+var answerbtn1 = document.createElement("button");
+answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
+answerbtn1.innerHTML = questions[2].answers[i].text;
+answerbtn1.value = questions[2].answers[i].correct;
+answerbtn1.setAttribute("id", questions[2].answers[i].text);
+document.getElementById("answers").appendChild(answerbtn1);
+}
+
+//IF FINAL QUESTIONS ARE ANSWERED
+if (
+document.getElementById(".EXAMPLE").addEventListener("click", function () {gameover()}),
+document.getElementById(".EXAMPLE").addEventListener("click", function () {rightAns()}),
+document.getElementById("#EXAMPLE").addEventListener("click", function () {gameover()}),
+document.getElementById("#EXAMPLE").addEventListener("click", function () {wrongAns()}),
+document.getElementById("EXAMPLE").addEventListener("click", function () {gameover()}),
+document.getElementById("EXAMPLE").addEventListener("click", function () {wrongAns()})
+);
+
+function gameover(){
+    document.getElementById("questionTitle").classList.add("hide");
+    document.getElementById(".EXAMPLE").classList.add("hide");
+    document.getElementById("#EXAMPLE").classList.add("hide");
+    document.getElementById("EXAMPLE").classList.add("hide");
+    scoreBoard.classList.remove("hide");
+    highscoreInput.classList.remove("hide");
+    saveHighscore.classList.remove("hide");
+}
 
 
 
-    // MOVES TO NEXT SET OF QUESTIONS
-    function nextQues() {
 
-        document.getElementById("HTML").classList.add("hide");
-        document.getElementById("CSS").classList.add("hide");
-        document.getElementById("ENGLISH").classList.add("hide");
-            for (let i = 0; i < 3; i++) {
-                document.getElementById("questionTitle").innerHTML = questions[1].question;
-                var answerbtn1 = document.createElement("button");
-                answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-                answerbtn1.innerHTML = questions[1].answers[i].text;
-                answerbtn1.value = questions[1].answers[i].correct;
-                answerbtn1.setAttribute("id", questions[1].answers[i].text);
-                document.getElementById("answers").appendChild(answerbtn1);
-            }
 
-            if (
-                document.getElementById("H1").addEventListener("click", function () {rightAns()},function(){nextQues}),
-                document.getElementById("P").addEventListener("click", function () {wrongAns()},function(){nextQues}),
-                document.getElementById("DIV").addEventListener("click", function () {wrongAns()},function(){nextQues2})
-            );
-
-            // MOVES TO NEXT SET OF QUESTIONS
-            function nextQues2() {
-                document.getElementById("H1").classList.add("hide");
-                document.getElementById("P").classList.add("hide");
-                document.getElementById("DIV").classList.add("hide");
-
-                for (let i = 0; i < 3; i++) {
-                    document.getElementById("questionTitle").innerHTML = questions[2].question;
-                    var answerbtn1 = document.createElement("button");
-                    answerbtn1.classList.add("btn", "btn-secondary", "answerbtn");
-                    answerbtn1.innerHTML = questions[2].answers[i].text;
-                    answerbtn1.value = questions[2].answers[i].correct;
-                    answerbtn1.setAttribute("id", questions[2].answers[i].text);
-                    document.getElementById("answers").appendChild(answerbtn1);
-                }
-
-                if (
-                    document.getElementById(".EXAMPLE").addEventListener("click", function () {rightAns()},function(){gameover}),
-                    document.getElementById("#EXAMPLE").addEventListener("click", function () {wrongAns()},function(){gameover}),
-                    document.getElementById("EXAMPLE").addEventListener("click", function () {wrongAns()},function(){gameover})
-                );
-
-            } //END OF nextQues2 FUNCTION
-    } // END OF nextQues FUNCTION
+} //END OF nextQues2 FUNCTION
+} // END OF nextQues FUNCTION
 } //END OF THEQUIZ FUNCTION
 
 
